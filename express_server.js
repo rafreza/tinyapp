adb1494bb2c5fb77d2cc1dffbbb87c9b6f281c2a
Rafin Reza <rafintokyo@gmail.com>
2021-01-20 23:54:02 +0000

    Redirects shortURL link to longURL's website

--- express_server.js
+++ express_server.js
@@ -25,13 +25,17 @@ app.get("/", (req, res) => { //Home
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => { //JSON string for URLDatabase
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
@@ -40,20 +44,25 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL) res.redirect(longURL);
+  else {
+    res.statusCode = 404;
+    res.send('<h2>404 Not Found!<br>URL does not exist.</h2>');
+  }
+})
 
 
-app.get("/urls.json", (req, res) => { //JSON string for URLDatabase
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

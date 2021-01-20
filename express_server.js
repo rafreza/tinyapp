90699a33b55feee4e4086c11b77b7cd4948e3284
Rafin Reza <rafintokyo@gmail.com>
2021-01-20 23:42:13 +0000

    Takes new URL submission, generates random shortURL and redirects to new url page

--- express_server.js
+++ express_server.js
@@ -35,8 +35,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {

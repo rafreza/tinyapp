d1c41c60c1904d9ae0fceeb16b94425551947d0e
Rafin Reza <rafintokyo@gmail.com>
2021-01-22 20:34:47 +0000

    Comments

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// Dependany imports
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -12,22 +13,25 @@ const bcrypt = require('bcrypt');
 
 app.set('view engine', 'ejs');
 
+//Functions from helpers.js
 const { getUserByEmail, generateRandomString, urlsForUser } = require('./helpers');
 
+// Object Databases
 const urlDatabase = {};
 const users = {};
 
 
-/* Sends responses based on URL path */
+////////////// ROUTING ///////////////
 
-app.get('/', (req, res) => { //Home
+// Home directory, redirects to URLs if logged in, otherwise redirects to registration page
+app.get('/', (req, res) => {
   if (req.session.userID) {
     res.redirect('/urls');
   } else {
     res.redirect('/login');
   }
 });
-
+// URL index page that shows all URLs belonging to user after logging in
 app.get('/urls', (req,res) => {
   const userID = req.session.userID;
   const userURLs = urlsForUser(userID, urlDatabase);
@@ -37,7 +41,7 @@ app.get('/urls', (req,res) => {
   
   res.render('urls_index', templateVars);
 });
-
+// URL index page after new URL is added to the database
 app.post('/urls', (req, res) => {
   if (req.session.userID) {
     const shortURL = generateRandomString();
@@ -45,14 +49,14 @@ app.post('/urls', (req, res) => {
       longURL: req.body.longURL,
       userID: req.session.userID
     };
-    res.redirect(`/urls/${shortURL}`);
+    res.redirect(`/urls/${shortURL}`); //redirects to short URL page
   } else {
     const errorMessage = 'Please login to view your new URLs.';
     res.status(401).render('urls_error', {user: users[req.session.userID], errorMessage});
   }
   
 });
-
+// Displays page after checking user login status
 app.get('/urls/new', (req, res) => {
   if (req.session.userID) {
     const templateVars = {user: users[req.session.userID]};
@@ -61,7 +65,7 @@ app.get('/urls/new', (req, res) => {
     res.redirect('/login');
   }
 });
-
+//Presents URL details
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const userID = req.session.userID;
@@ -79,7 +83,7 @@ app.get('/urls/:shortURL', (req, res) => {
   }
   
 });
-
+//Updates long URL
 app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   if (req.session.userID && req.session.userID === urlDatabase[shortURL].userID) {
@@ -91,7 +95,7 @@ app.post('/urls/:shortURL', (req, res) => {
   }
   
 });
-
+//If link belongs to user, deletes selected URL from database and page
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   if (req.session.userID && req.session.userID === urlDatabase[shortURL].userID) {
@@ -103,7 +107,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   }
   
 });
-
+//Redirects to long URL page
 app.get('/u/:shortURL', (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     res.redirect(urlDatabase[req.params.shortURL].longURL);
@@ -114,7 +118,7 @@ app.get('/u/:shortURL', (req, res) => {
   }
 });
 
-// login page & functionality
+// If logged in, redirects to index page
 app.get('/login', (req, res) => {
   if (req.session.userID) {
     res.redirect('/urls');
@@ -124,7 +128,7 @@ app.get('/login', (req, res) => {
   const templateVars = {user: users[req.session.userID]};
   res.render('urls_login', templateVars);
 });
-
+//If credentials are valid, redirects to index page
 app.post('/login', (req, res) => {
   const user = getUserByEmail(req.body.email, users);
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
@@ -135,12 +139,12 @@ app.post('/login', (req, res) => {
     res.status(401).render('urls_error', {user: users[req.session.userID], errorMessage});
   }
 });
-
+//Logs user out and clears cookies, redirects to index page
 app.post('/logout', (req, res) => {
   res.clearCookie('session');
   res.redirect('session.sig');
 });
-
+//After login, redirects to URLs index page
 app.get('/register', (req, res) => {
   if (req.session.userID) {
     res.redirect('/urls');
@@ -150,7 +154,7 @@ app.get('/register', (req, res) => {
   const templateVars = {user: users[req.session.userID]};
   res.render('urls_registration', templateVars);
 });
-
+// After registration, if credentials are valid redirects to URLs index page
 app.post('/register', (req, res) => {
   if (req.body.email && req.body.password) {
     if (!getUserByEmail(req.body.email, users)) {
@@ -172,7 +176,7 @@ app.post('/register', (req, res) => {
   }
 });
 
-
+// Server consoles that it is listening once connected
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
